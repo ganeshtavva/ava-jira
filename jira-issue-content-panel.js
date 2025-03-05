7a3dcbb9e53540e6156314e36972edc71c89c63d
ganeshtavva <ganesh.tavva@gmail.com>
2025-03-05 20:10:41 +0530

    Update jira-issue-content-panel.js

--- jira-issue-content-panel.js
+++ jira-issue-content-panel.js
@@ -246,29 +246,10 @@ async function  handleTestCaseClick(project,epicId,storyId, storyDescription) {
  AP.flag.create({
   title: 'Notification',
   body: 'This is a message for the user!',
-  type: 'info' // Types: 'info', 'success', 'warning', 'error'
-});
-AP.inlineDialog.create({
-  key: "my-inline-dialog",
-  width: "300px",
-  height: "auto",
-  target: "my-button",
-  hideOnUnfocus: true,
-  chrome: true,
-  onHover: false,
-  onShow: function(dialog) {
-    dialog.setBody(`
-      <div style="padding: 10px; font-family: Arial, sans-serif;">
-        <strong>Your team is working in Confluence</strong>
-        <p>Don’t miss out on the conversations happening between your collaborators!</p>
-        <a href="https://www.atlassian.com/confluence" target="_blank">Access Confluence for free</a>
-      </div>
-    `);
-  }
+  type: 'success' // Types: 'info', 'success', 'warning', 'error'
 });
 
-  
-   console.log("Payload:", payload);
+console.log("Payload:", payload);
  }
  async function fetchUserCredentials() {
   const credentialUrl = `${API_BASE_URL}/ava/force/credential?jiraUserUrl=${jiraUserUrl}`;
